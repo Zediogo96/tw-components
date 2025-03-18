deef92695f5725a6c980ef1205b3ba2de169ae29
José Diogo <josediogo96@outlook.com>
2025-03-18 16:14:35 +0000

    logo horizontal infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import SkewedInfiniteScroll from "./components/skewed-infinite-scroll";
+import LogosInfiniteScroll from "./components/logos-infinite-scroll";
 
 // src/app.jsx
 export default function App() {
   return (
-    <div className="bg-slate-50 h-screen w-screen flex items-center justify-center px-16">
+    <div className="bg-slate-500 h-screen w-screen flex items-center justify-center px-16">
       <div className="relative w-full max-w-lg">
-        <SkewedInfiniteScroll />
+        <LogosInfiniteScroll />
       </div>
     </div>
   )
