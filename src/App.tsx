a3a539947ed1650e2e90b5dac83567d4b9d12ef1
José Diogo <josediogo96@outlook.com>
2025-03-18 15:57:08 +0000

    skewed infinite scroll animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import BlendMovingBackground from "./components/blend-mobing-bg";
+import SkewedInfiniteScroll from "./components/skewed-infinite-scroll";
 
 // src/app.jsx
 export default function App() {
   return (
     <div className="bg-slate-50 h-screen w-screen flex items-center justify-center px-16">
       <div className="relative w-full max-w-lg">
-        <BlendMovingBackground />
+        <SkewedInfiniteScroll />
       </div>
     </div>
   )
